d89c26fcfd1bed9742130326fba4e7573f3b1192
eni <erchang.ni@epfl.ch>
2023-04-19 17:58:37 +0200

    finish collaborative filtering

--- src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
+++ src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
@@ -1,6 +1,5 @@
 package app.recommender.collaborativeFiltering
-
-
+import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 import org.apache.spark.rdd.RDD
 
 class CollaborativeFiltering(rank: Int,
@@ -10,10 +9,15 @@ class CollaborativeFiltering(rank: Int,
 
   // NOTE: set the parameters according to the project description to get reproducible (deterministic) results.
   private val maxIterations = 20
-  private var model = null
+  private var model: MatrixFactorizationModel = _
 
-  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = {
+    val ratings = ratingsRDD.map { case (user_id, movie_id, _, rating, _) => Rating(user_id, movie_id, rating) }
+    model = ALS.train(ratings, rank, maxIterations, regularizationParameter, n_parallel, seed)
+  }
 
-  def predict(userId: Int, movieId: Int): Double = ???
+  def predict(userId: Int, movieId: Int): Double = {
+    model.predict(userId, movieId)
+  }
 
 }
